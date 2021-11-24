4982220ef1f8eb3a7ddbcf45763cd3b975928cf9
samini1 <92944248+samini1@users.noreply.github.com>
2021-11-24 17:09:03 -0600

    added arrays to represent possible character types to pull from, attempting to create or loop which adds 1 character to a string at a time? need to check if checkbox state checked works to validate and include into password array, check if math.random can be used in loop to select array values

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,65 @@
 // Assignment code here
+var formEl = document.querySelector("#criteria-form");
+
+var criteriaFormHandler = function(event) {
+  event.preventDefault();
+
+  //enter input values from form
+  var criteriaLengthInput = document.querySelector("input[name='password-length']").value;
+  //try to make length input limit entries from 8-128? Figure out if have time later
+  var criteriaLowerInput = document.querySelector("input[name='lowercase']").value;
+  var criteriaUpperInput = document.querySelector("input[name='uppercase']").value;
+  var criteriaNumericInput = document.querySelector("input[name='numeric']").value;
+  var criteriaSpecialInput = document.querySelector("input[name='special']").value;
+
+  //validate inputs
+
+  if (criteriaLengthInput === "" || criteriaLengthInput < 8 ||criteriaLengthInput > 128) {
+    alert("Please enter a valid length between 8 and 128");
+    return false;
+  } else if (criteriaLowerInput ==="" && criteriaUpperInput ==="" && criteriaNumericInput ==="" && criteriaSpecialInput==="") {
+    alert("Please select at least one character criteria in order to generate your password");
+    return false;
+  }
+  
+  
+  
+   //this stuff goes somewhere??
+
+   var lowerChars=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperChars=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var numericChars=['0','1','2','3','4','5','6','7','8','9'];
+  var specialChars=['!','@','#','$','%','&', '-', '=', '+'];
+  var passChars=[];
+
+//loop to generate password once an array has been created
+
+
+
+//need to add chracters to some kind of array to access
+  if (criteriaUpperInput.checked === true) {
+    passChars=upperChars.concat(passChars);    
+  } 
+  if(criteriaSpecialInput.checked === true) {
+    passChars=specialChars.concat(passChars);
+  }
+  if (criteriaLowerInput.checked === true) {
+    passChars=lowerChars.concat(passChars);
+  }
+  if (criteriaNumericInput.checked === true) {
+    passChars=numericChars.concat(passChars);
+  }
+    
+console.log(passChars);
+  
+for ( var i=0; i < criteriaLengthInput; i++) {
+  //iterate by selecting random character from passChars array
+  //use Math object to select a random array position by multiplying by the chosen length.
+  //how to make Math array
+Math.floor(Math.random()*passChars.length);
+}  
+
+}
 
 
 // Get references to the #generate element
@@ -10,6 +71,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Password Generator Starter Code
+# Password Generator
+
+#This application should randomly generate a password that provides greater security.
+
+## Needed functionality
+
+clickButton creates prompts for password criteria user can select
+
+promptLength --> user chooses minimum 8, maximum 128 characters. 
+    lengthError- user chooses less than 8 or more than 128, allow user to try again.
+
+if at least one character type prompt is confirmed, password generator should run;
+else, prompt user to return to start of loop;
+
+generatePassword --> 
+    checks character length
+    checks to include lowercase
+    checks to include uppercase
+    checks to unclude Numeric
+    checks to include Special
+    randomly generates password from these arrays.
+        How do I do this?
+        Needs to check if at least 1 from each confirmed type is included?
