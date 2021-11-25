5c0e3ae3b7c7f8e678ad17cf23e42491e31d7fac
samini1 <92944248+samini1@users.noreply.github.com>
2021-11-25 15:19:48 -0600

    verified that checking boxes pushes the appropriate arrays together next step is to rig a loop to randomly select characters from the array

--- Develop/script.js
+++ Develop/script.js
@@ -8,22 +8,48 @@ var criteriaFormHandler = function(event) {
   event.preventDefault();
 
   //enter input values from form
-  var criteriaLengthInput = document.querySelector("input[name='password-length']").value;
+  var lengthInput = document.querySelector("input[name='password-length']").value;
   //try to make length input limit entries from 8-128? Figure out if have time later
-  var criteriaLowCheck = document.querySelector("#lowercase");
-  var criteriaUppCheck = document.querySelector("#uppercase");
-  var criteriaNumCheck = document.querySelector("#numeric");
-  var criteriaSpCheck = document.querySelector("#special");
+  var lowCheck = document.querySelector("#lowercase");
+  var uppCheck = document.querySelector("#uppercase");
+  var numCheck = document.querySelector("#numeric");
+  var spCheck = document.querySelector("#special");
 
+    //set arrays to reference according to values chosen in the form. Maybe do that here?
+    var lowerChars=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var upperChars=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var numericChars=['0','1','2','3','4','5','6','7','8','9'];
+    var specialChars=['!','@','#','$','%','&', '-', '=', '+'];
+    var passChars=[];
   //validate inputs
 
-  if (criteriaLengthInput === "" || criteriaLengthInput < 8 ||criteriaLengthInput > 128) {
+  //how do I get these values into the generatePassword function to check this? Create an object to pass into function?
+if (uppCheck.checked === true) {
+  passChars=upperChars.concat(passChars);    
+} 
+if(spCheck.checked === true) {
+  passChars=specialChars.concat(passChars);
+}
+if (lowCheck.checked === true) {
+  passChars=lowerChars.concat(passChars);
+}
+if (numCheck.checked === true) {
+  passChars=numericChars.concat(passChars);
+}
+
+console.log(passChars);
+
+  if (lengthInput === "" || lengthInput < 8 ||lengthInput > 128) {
     alert("Please enter a valid length between 8 and 128");
     
-  } else if (criteriaLowCheck.checked ===false && criteriaUppCheck.checked ===false && criteriaNumCheck.checked ===false && criteriaSpCheck.checked===false) {
+  } else if (lowCheck.checked ===false && uppCheck.checked ===false && numCheck.checked ===false && spCheck.checked===false) {
     alert("Please select at least one character criteria in order to generate your password");
+
     
-  }
+    
+//Form check works, now pass that information into the password generator
+
+  } 
 }
 var generateBtn = document.querySelector("#generate");
   generateBtn.addEventListener("click", criteriaFormHandler);
@@ -39,34 +65,14 @@ var generateBtn = document.querySelector("#generate");
 //   //this needs to be able to reference the values input into the form, determine which character arrays to add to the password, use a loop to select random values out of the password array and
 //   //attach them to the password text
 
-//   //set arrays to reference according to values chosen in the form. Maybe do that here?
-//   var lowerChars=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-//   var upperChars=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-//   var numericChars=['0','1','2','3','4','5','6','7','8','9'];
-//   var specialChars=['!','@','#','$','%','&', '-', '=', '+'];
-//   var passChars=[];
 
 
-// //how do I get these values into the generatePassword function to check this? Create an object to pass into function?
-// if (criteriaUppCheck.checked === true) {
-//   passChars=upperChars.concat(passChars);    
-// } 
-// if(criteriaSpCheck.checked === true) {
-//   passChars=specialChars.concat(passChars);
-// }
-// if (criteriaLowCheck.checked === true) {
-//   passChars=lowerChars.concat(passChars);
-// }
-// if (criteriaNumCheck.checked === true) {
-//   passChars=numericChars.concat(passChars);
-// }
 
-// }
-// console.log(passChars);
+
  
 // //once I figure out how to pass these values in to be checked, create the loop to select random values
 
-// for ( var i=0; i < criteriaLengthInput; i++) {
+// for ( var i=0; i < lengthInput; i++) {
 //   //iterate by selecting random character from passChars array
 //   //use Math object to select a random array position by multiplying by the chosen length.
 //   //how to make Math array; var password = then add each character to password text?
