5301e8df169f7a32ad61f9c3d9aab7bdc7fe06f0
samini1 <92944248+samini1@users.noreply.github.com>
2021-11-26 21:59:38 -0600

    Application works now. Fix some appearance issues before final submission.

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var formEl = document.querySelector("#password-form");
 //select all of the check boxes
 var checkForms = formEl.querySelectorAll('input[type="checkbox');
 
-var criteriaFormHandler = function(event) {
+var writePassword = function(event) {
   event.preventDefault();
 
   //enter input values from form
@@ -15,7 +15,7 @@ var criteriaFormHandler = function(event) {
   var numCheck = document.querySelector("#numeric");
   var spCheck = document.querySelector("#special");
 
-    //set arrays to reference according to values chosen in the form. Maybe do that here?
+      //set arrays to reference according to values chosen in the form. 
     var lowerChars=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     var upperChars=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     var numericChars=['0','1','2','3','4','5','6','7','8','9'];
@@ -44,64 +44,18 @@ if (numCheck.checked === true) {
     alert("Please select at least one character criteria in order to generate your password");    
    } 
 
-   var passString=
-
-
-//Form check works, now need to create a loop to generate the password
+  //create an empty string to assign password characters
+  var password = "";
+  //grab a random character from the array and add it to password
+  for (var i = 0 ; i < lengthInput; i++) {
+    var passCharacter = passChars[Math.floor(Math.random()*passChars.length)];
+    password=password + passCharacter;
+  }
+  
 var passwordText = document.querySelector("#password");
-passwordText.value = "Gibbity Goo";
-
-
-// for ( var i=0; i < lengthInput; i++) {
-//   //iterate by selecting random character from passChars array
-//   //use Math object to select a random array position by multiplying by the chosen length.
-//   //how to make Math array; var password = then add each character to password text?
-// var password = Math.floor(Math.random()*passChars.length);
-// }  
+passwordText.value = password;
    
 }
 var generateBtn = document.querySelector("#generate");
-  generateBtn.addEventListener("click", criteriaFormHandler);
+  generateBtn.addEventListener("click", writePassword);
   
-   //this stuff goes somewhere??
-
- 
-
-// //loop to generate password once an array has been created
-
-
-// var generatePassword = function(passDataObject) {
-//   //this needs to be able to reference the values input into the form, determine which character arrays to add to the password, use a loop to select random values out of the password array and
-//   //attach them to the password text
-
-
-
-
-
- 
-//once I figure out how to pass these values in to be checked, create the loop to select random values
-
-
-
-// }
-
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   //this tells generate Password function to run and, well generate the password variable
-//   var password = generatePassword();
-
-//   //this references the password id, then states that the value property of the passwordText object is password
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-
-// }
-
-// // Add event listener to generate button
-// //on button click this should run the writePassword function
-// generateBtn.addEventListener("click", writePassword);
